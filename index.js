2423ff7c7525007952d23f983b72e63e7aab249a
Erik Björn <erik.g.bjoern@gmail.com>
2020-10-09 19:02:46 +0200

    adding Swedish page as well, toggle between EN/SE

---
+++ index.js
@@ -0,0 +1,53 @@
+const bodyClass = document.getElementsByTagName('body')[0].classList
+const headerClass = document.getElementsByTagName('header')[0].classList
+const languageSwitchClass = document.getElementsByClassName('language-switch')[0].classList
+let mouseOver = false
+let mouseOut = false
+
+const collapseHeader = () => {
+  if (!mouseOut && headerClass.contains('expanded-header')) {
+    mouseOver = false
+    mouseOut = true
+    headerClass.add('collapsed-header')
+    headerClass.remove('expanded-header')
+    bodyClass.remove('dim')
+    languageSwitchClass.add('collapsed')
+  }
+}
+
+const expandHeader = () => {
+  if (!mouseOver && headerClass.contains('collapsed-header')) {
+    mouseOver = true
+    mouseOut = false
+    headerClass.add('expanded-header')
+    headerClass.remove('collapsed-header')
+    bodyClass.add('dim')
+    languageSwitchClass.remove('collapsed')
+
+    setTimeout(() => {
+      mouseOver = false
+    }, 1000)
+  }
+}
+
+const keepHeaderOpenOnSmallScreens = () => {
+  if (window.innerWidth <= 520) {
+    expandHeader()
+  }
+}
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY >= 15) {
+    headerClass.remove('expanded-header')
+    headerClass.add('collapsed-header')
+    bodyClass.remove('dim')
+    languageSwitchClass.remove('top-position')
+    languageSwitchClass.add('collapsed')
+  }
+  if (window.scrollY <= 1) {
+    headerClass.remove('collapsed-header')
+    headerClass.remove('expanded-header')
+    languageSwitchClass.remove('collapsed')
+    languageSwitchClass.add('top-position')
+  } 
+})
